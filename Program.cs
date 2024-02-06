98f939dd180395d20e8193f720c91f6f838646b5
micdra01 <114472076+micdra01@users.noreply.github.com>
2024-02-06 19:21:21 +0100

    Drone test

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(Greeting());
+    }
+
+    public static string Greeting()
+    {
+        bool isDroneCI = Environment.GetEnvironmentVariable("DRONE") == "true";
+
+        if (isDroneCI)
+        {
+            return "Hello, Drone CI!";
+        }
+        else
+        {
+            return "Hello, World!";
+        }
+    }
+}
+
---
+++ Test.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace MichaelDrone;
+
+public class Tests
+{
+    [Test]
+    public void IsDroneTest()
+    {
+        Environment.SetEnvironmentVariable("DRONE", "true");
+
+        var result = Program.Greeting();
+
+        Console.WriteLine(result);
+        Assert.That(result.Equals("Hello, Drone CI!"));
+    }
+}
